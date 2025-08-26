d25e18f916192f007762583edd654afb47e6e70a
ynhj <542182536@qq.com>
2025-08-26 21:21:53 +0800

    feat: update code

--- backend/src/main/java/com/example/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/com/example/ecommerce/EcommerceApplication.java
@@ -1,10 +1,11 @@
 package com.example.ecommerce;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan(basePackages = "com.example.ecommerce.mapper")
 public class EcommerceApplication {
 
     public static void main(String[] args) {
